b5afed1003b1b40c308d48f549507f529f0c7488
Houjin Zhang <shandonghoujin@163.com>
2021-03-04 10:24:28 +0800

    Get Json Data

--- ESP8266/NodeMCU_OneNET/NodeMCU_OneNET.ino
+++ ESP8266/NodeMCU_OneNET/NodeMCU_OneNET.ino
@@ -1,7 +1,9 @@
 // Upload data using OneNET + NodeMCU
-// Test By ZhangHoujin 2021.03.03
+// Test By ZhangHoujin 2021.03.04
+
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 #define DEVICE_ID "xxxxxxxxxxxxxx"
 #define  API_Key  "xxxxxxxxxxxxxx"
 String   GET_URL = "http://api.heclouds.com/devices/" + String(DEVICE_ID) + "/datastreams/Temperature";
@@ -51,7 +53,8 @@ void ESP8266_GET(){
 
   if(httpCode == HTTP_CODE_OK){
     String responsePayload = httpClient.getString();
-    Serial.println("[GET] Server Response: " + responsePayload);
+    Json_Analysis_GET(responsePayload);
+    //Serial.println("[GET] Server Response: " + responsePayload);
     Serial.println("-------------------------------------------\n");
   }
   else{
@@ -83,4 +86,32 @@ void ESP8266_POST(){
   else{
     Serial.printf("[ERROR] Failed, POST error: %s\n", httpClient.errorToString(httpCode).c_str());
   }
+}
+
+void Json_Analysis_GET(String input){
+  StaticJsonDocument<256> doc;
+  DeserializationError error = deserializeJson(doc, input);
+  
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+  
+  int errno = doc["errno"]; // 0
+  JsonObject data = doc["data"];
+  const char* data_update_at = data["update_at"]; // "2021-03-04 09:10:00"
+  const char* data_id = data["id"]; // "Temperature"
+  const char* data_create_time = data["create_time"]; // "2021-03-03 22:26:07"
+  int data_current_value = data["current_value"]; // 24
+  const char* succ = doc["error"]; // "succ"
+  
+  Serial.println("[GET]     data_id: " + String(data_id));
+  Serial.println("[GET]  data_value: " + String(data_current_value));
+  Serial.println("[GET] update_time: " + String(data_update_at));
+  Serial.println("[GET] create_time: " + String(data_create_time));
+  if(succ == "succ"){
+    Serial.println("[GET] OK!");
+  }
+  
 }
